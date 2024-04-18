7fe1d190ffc42b9c4cdfdfbc2901d86e656fc4a0
Norbjz <norberto.j90@gmail.com>
2024-04-18 06:21:51 -0600

    Eliminado de instrucciones repetidas

--- ejercicio10.js
+++ ejercicio10.js
@@ -5,19 +5,6 @@ let data = [
   ["David", "Moranchel", 28],
 ];
 
-/*
-    1. Necesitamos una lista con los nombres completos de las personas que se encuentran en la data
-    2.- Necesitamos saber cual es la mayor y la menor edad de las personas en la lista
-    3.- Necesitamos reestructurar la data para que quede de la siguiente forma:
-
-    let restructuredData = [
-        {
-            name:
-            lastname:
-            age
-        }
-    ]
-  */
 //1. Necesitamos una lista con los nombres completos de las personas que se encuentran en la data
 
 const getFullNames = (data) =>
